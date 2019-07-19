aa7143ffb1a50f9469358a23ae3429debee080bd
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-07-18 19:33:53 -0700

    roxygen documentation of function

--- R/fbind.R
+++ R/fbind.R
@@ -1,4 +1,20 @@
 
+#' Bind factors nicely
+#'
+#' This is the description part.
+#'
+#' Here's another paragraph.
+#'
+#' @param a A factor.
+#' @param b A factor.
+#'
+#' @return A factor.
+#' @export
+#'
+#' @examples
+#' a <- factor(c("hi", "there"))
+#' b <- factor(c("what's", "up"))
+#' fbind(a, b)
 fbind <- function(a, b){
 
     factor(c(as.character(a),
